e7727fddf4c1531e2b82ddbcc20c39628b9c4815
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-04 08:31:39 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+# ----------------------------
+# Teffaform configuration
+# ----------------------------
+terraform {
+  required_version = ">=1.3.6"
+  required_providers {
+    aws = {
+      soursource = "hashicorp/aws"
+      version = ">=4.45.0"
+    }
+  }
+}
+
+# ----------------------------
+# Provider
+# ----------------------------
+provider "aws" {
+  profile = "terraform"
+  region = "ap-northeast-1"
+}
+
+# ----------------------------
+# variables
+# ----------------------------
+variable "project" {
+  type = string
+}
+variable "environment" {
+  type = string
+}
