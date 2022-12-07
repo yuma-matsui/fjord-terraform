f2a7690de50acfd9016343fac44d91d9fec9d2d6
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-07 09:00:18 +0900

    ACMを作成した

--- alb.tf
+++ alb.tf
@@ -24,6 +24,17 @@ resource "aws_lb_listener" "alb_listener_http" {
     target_group_arn = aws_alb_target_group.alb_target_group.arn
   }
 }
+resource "aws_lb_listener" "alb_listener_https" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.tokyo_cert.arn
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.alb_target_group.arn
+  }
+}
 
 # ----------------------------
 # target group
